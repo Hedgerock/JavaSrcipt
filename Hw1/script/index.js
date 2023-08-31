7d28f7f97d8a6096288395588dbefe9b5c9d618c
Dmitro Vasyliev <dmitriyvas21@gmail.com>
2023-08-31 12:34:48 +0300

    mission complete :)

---
+++ Hw1/script/index.js
@@ -0,0 +1,12 @@
+let firstNumber = 15;
+let secondNumber = 30;
+
+let sum = firstNumber + secondNumber;
+let difference = secondNumber - firstNumber;
+let product = firstNumber * secondNumber;
+let quotient = secondNumber / firstNumber;
+
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
